f2f008339501d37b94084e754056bcf307176c30
Orhan <zekic2487@gmail.com>
2023-11-27 17:44:13 +0100

    esercizio 3 finito

--- index.php
+++ index.php
@@ -36,7 +36,9 @@ foreach($users as $user){
 
 // ESERCIZIO 3
 
-// Scrivere un programma che stampi in console tutti i numeri da uno a cento. Se il numero è multiplo di 3 stampare “PHP” al posto del numero; se multiplo di 5 stampare “JAVASCRIPT”; se multiplo di 3 e 5 contemporaneamente deve stampare “HACKADEMY110".
+// Scrivere un programma che stampi in console tutti i numeri da uno a cento. 
+// Se il numero è multiplo di 3  stampare    PHP” al posto del numero;
+//  se multiplo di 5 stampare “JAVASCRIPT”; se multiplo di 3 e 5 contemporaneamente deve stampare “HACKADEMY110".
 
 $numbers = 1;
 for($i = 0; $i < 100; $i++) {
